ca91228e12d957d6e9da76fe6b188c10389b31f6
Jonas <jonas.ditz@tuebingen.mpg.de>
2016-04-07 13:49:53 +0200

    change license to GNU General Public License

--- configuration/fetchData.py
+++ configuration/fetchData.py
@@ -1,6 +1,28 @@
 #!/usr/bin/env python
 
-"""
+""" Data fetching script for MUGS
+This script provides a routine to fetch all data, 
+which are needed for the configuration of MUGS
+
+Copyright (c) 2013, 2016 Max Planck Institute for Biological Cybernetics
+All rights reserved.
+ 
+This file is part of MUGS - Mobile and Unrestrained Gazetracking Software.
+
+MUGS is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+MUGS is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with MUGS.  If not, see <http://www.gnu.org/licenses/>. 
+
+Author: Jonas Ditz (jonas.ditz@tuebingen.mpg.de)
 """
 
 import sys
@@ -10,6 +32,9 @@ import pylsl
 # Constants
 # ----------------------------------------
 
+DOT_STREAM_NAME = 'ConfigurationDotPositions'
+EYE_STREAM_NAME = ''
+HEAD_STREAM_NAME = ''
 
 # ----------------------------------------
 # Classes
@@ -20,35 +45,50 @@ import pylsl
 # Functions
 # ----------------------------------------
 
-def fetchDotPositions(lslInlet):
-    """
-    """
-    return False
+def fetchLSLstream(name):
+    """ Try to fetch a LSL stream with a specific name.
 
-def fetchHeadData(lslInlet):
+        Keyword arguments:
+        name - Value of the 'name' property of the stream.
     """
-    """
-    return False
+    results = pylsl.resolve_byprop('name', name)
+    
+    if (len(results) == 0):
+        print 'Could not find any stream with the specified name: ',name
+        sys.exit()
 
-def fetchEyeData(lslInlet):
-    """
-    """
-    return False
+    lslInlet = pylsl.StreamInlet(results[0])
+    return lslInlet
 
 
 if __name__ == '__main__':
-    print 'resolving streams...'
-    results = pylsl.resolve_byprop('name', 'ConfigurationDotPositions')
-    #result = pylsl.resolve_byprop('type', 'TXT', 1, 20)
-    print 'done'
+    # Fetch the stream with eye tracker data.
+    print 'fetching Eye tracker stream...'
+    #eyeInlet = fetchLSLstream(EYE_STREAM_NAME)
+    print 'DONE'
 
-    if (len(results) == 0):
-        print 'Could not find any stream with the spezified properties'
-        sys.exit()
+    # Fetch the stream with head tracker data.
+    print 'fetching Head tracker stream...'
+    #headInlet = fetchLSLstream(HEAD_STREAM_NAME)
+    print 'DONE'
+    
+    # Fetch the stream with dot positions.
+    print 'fetching dot position stream...'
+    dotInlet = fetchLSLstream(DOT_STREAM_NAME)
+    print 'DONE'
 
-    inlet = pylsl.StreamInlet(results[0])
 
-    sample, timestamp = inlet.pull_sample()
-    while (sample != None and timestamp != None):
-        print timestamp,sample
-        sample, timestamp = inlet.pull_sample()
+    # Data is stored an 2-dimentional list with the following layout:
+    # [[timestamp, posHead, orientHead, leftEye, rightEye, posTarget]]
+    data = []
+    while (True):
+        dotSample, dotTimestamp = dotInlet.pull_sample()
+        #eyeSample, eyeTimestamp = eyeInlet.pull_sample()
+        #headSample, headTimestamp = headInlet.pull_sample()
+        eyeSample = [0,0,0,0]
+        headSample = [0,0,0,0,0,0]
+
+        data.append([dotTimestamp, headSample[0], headSample[1], 
+                     headSample[2], headSample[3], headSample[4], 
+                     headSample[5], eyeSample[0], eyeSample[1], 
+                     eyeSample[2], eyeSample[3], dotSample[0], dotSample[1]])
